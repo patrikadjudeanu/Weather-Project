929e06226ec35c5fe9c3ffbe7adf8fe6c73a290e
patrikadjudeanu <patrikadjudeanu@yahoo.com>
2020-08-13 16:51:34 +0300

    Updated home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,62 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="card-header">
+                    <div class = "row justify-content-between">
+                        <div style = "padding-left:10px; padding-top:5px; font-size:20px">
+                            Hello, {{ Auth::user()->name }}!
                         </div>
-                    @endif
+                        <div style = "padding-right:10px">
+                            <a class = "btn btn-danger" href = "{{ route('home') }}">
+                            <!-- ROUTE LOGOUT -->
+                                Log out
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
-                    {{ __('You are logged in!') }}
+                <div class="card-body" style = "font-size:20px; padding-top:40px">
+                    <div>
+                        <b>Your current location:</b>
+                    </div>
+                    <div>
+                        <!-- GEOLOCATION API -->
+                        TODO GEO API
+                    </div>
+                    <div class = "row"   style = "padding-top:10px">
+                        <div class = "col">
+                            <div>
+                                <b>Current temperature:</b>
+                            </div>
+                            <div>
+                                <!-- WEAHER API -->
+                                TODO WEATHER API
+                            </div>
+                        </div>
+                        <div class = "col">
+                            <div>
+                                Updated at:
+                                <!-- WEATHER API UPDATE TIMESTAMP -->TODO Timestamp
+                            </div>
+                            <div>
+                                <button class = "btn btn-info btn-sm" id = "resetCurWeatherBtn">Reset</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style = "padding-top: 35px" class = "row">
+                        <div class="col-md-3 offset-md-2">
+                            <a class = "btn btn-primary" href = "{{ route('home') }}">
+                                <!-- ROUTE TEMPERATURES -->
+                                Get temperatures
+                            </a>
+                        </div>
+                        <div class="col-md-3 offset-md-2">
+                            <a class = "btn btn-primary" href = "{{ route('home') }}">
+                                <!-- ROUTE STATISTICS -->
+                                Statistics
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
